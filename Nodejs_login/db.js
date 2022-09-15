a58288ddc4185f6a3a9a0b15b570413727344126
Clara Carvalho <clara.coutinho@escolar.ifrn.edu.br>
2022-09-15 19:07:47 -0300

    Commit inicial

---
+++ Nodejs_login/db.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize("outbreak", "root", "Info@1234", {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+sequelize.authenticate()
+.then(function(){
+    console.log("Conexão com o banco de dados realizada com sucesso!")
+}).catch(function(){
+    console.log("Erro: Conexão com o banco de dados não realizada!")
+})
+
+
+
+module.exports = sequelize;
+
---
+++ Nodejs_login/server.js
@@ -0,0 +1,101 @@
+(async () => {
+    const db = require('./db');
+    const Narrativa = require('./narrativa')
+
+    try {
+        const resultado = await db.sync();
+        console.log(resultado)
+    } catch (error) {
+        console.log(error);
+    }
+
+    const resultadoCreate = await Narrativa.create({
+        historia: 'fanfic vampiro'
+    })
+    console.log(resultadoCreate);
+
+    const narrativa = await Narrativa.findByPk(1);
+    console.log(narrativa);
+    narrativa.historia = "nova história";
+    
+
+    const resultadoSave = await narrativa.save();
+    console.log(resultadoSave);
+   
+})();
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const db = require('./db');
+const Narrativa = require('./narrativa');
+
+app.use(express.static('public'));
+app.use(express.json());
+    app.use(express.urlencoded({ extended: false}))
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'))
+})
+
+app.get('/cadastro', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'cadastro.html'))
+})
+app.get("/", async (req, res) => {
+    res.send("Página inicial - Outbreak")
+})
+app.get('/feed', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'feed.html'))
+})
+
+app.post('/cadastro-form', (req, res) => {
+    const email = req.body.email
+    const username = req.body.username
+    const password = req.body.password
+     console.log({
+         email,
+         username,
+         password
+     })
+    res.send({
+        email,
+        username,
+        password
+    })
+})
+
+app.post('/login-form', (req, res) => {
+    const email = req.body.email
+    const username = req.body.username
+    const password = req.body.password
+     console.log({
+         email,
+         username,
+         password
+     })
+    res.send({
+        email,
+        username,
+        password
+    })
+})
+
+app.post('/feed-form', (req,res) => {
+    const Narrativa = req.body.Narrativa
+     console.log({
+         Narrativa
+     })
+     res.send({
+         Narrativa
+     })
+})
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000...')
+});
+
+    
