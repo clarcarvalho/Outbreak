c14c0167e2238a0492d87694f5301442d93d9469
Vinícios Barbosa <viniciosbarbosa740@gmail.com>
2022-09-16 16:44:44 -0300

    feita uma refatpração nos arquivos do projeto

---
+++ src/models/usuario.js
@@ -0,0 +1,25 @@
+const Sequelize = require("sequelize");
+const db = require("../config/db");
+
+const Usuario = db.define("usuario", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  nome: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Usuario;
